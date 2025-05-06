5320b99b6244a3b2dc44824710f9169cb2945ac8
lennon-jesus <161793432+lennon-jesus@users.noreply.github.com>
2025-05-06 13:09:57 -0300

    Add files via upload

---
+++ public/js/atendimentoScript.js
@@ -0,0 +1,64 @@
+const prevBtns = document.querySelectorAll(".btn-prev");
+const nextBtns = document.querySelectorAll(".btn-next");
+const progress = document.getElementById("progress");
+const formSteps = document.querySelectorAll(".form-step");
+const progressSteps = document.querySelectorAll(".progress-step");
+
+let formStepsNum = 0;
+
+// Quantidade de hóspedes
+function changeQnt(){
+    qnt = document.getElementById("qntInput");
+    if(qnt.value>9)
+        qnt.value=9;
+    else if(qnt.value<1)
+        qnt.value=1;
+}
+
+// Número do documento
+function clearDoc(idDoc){
+    document.getElementById(idDoc).value='';
+}
+
+// Páginas
+nextBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepsNum++;
+    updateFormSteps();
+    updateProgressbar();
+  });
+});
+
+prevBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    formStepsNum--;
+    updateFormSteps();
+    updateProgressbar();
+  });
+});
+
+function updateFormSteps() {
+  formSteps.forEach((formStep) => {
+    formStep.classList.contains("form-step-active") &&
+      formStep.classList.remove("form-step-active");
+  });
+
+  formSteps[formStepsNum].classList.add("form-step-active");
+}
+
+
+// Barra de progresso
+function updateProgressbar() {
+  progressSteps.forEach((progressStep, idx) => {
+    if (idx < formStepsNum + 1) {
+      progressStep.classList.add("progress-step-active");
+    } else {
+      progressStep.classList.remove("progress-step-active");
+    }
+  });
+
+  const progressActive = document.querySelectorAll(".progress-step-active");
+
+  progress.style.width =
+    ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
+}
